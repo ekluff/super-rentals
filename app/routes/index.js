1f3d75df231eee60159e3a5473d3f04d87156049
Evan Clough & amit mhatre <student@epicodus.com>
2015-09-21 14:10:41 -0700

    Add delete button and routes to index view.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
+
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+
 });
